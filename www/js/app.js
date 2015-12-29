114a623d3524e50b71a26e31ceffa1137efd21ab
fatbigbright <bigbrightf@gmail.com>
2015-12-29 14:18:47 +0800

    jumping between views

--- www/js/app.js
+++ www/js/app.js
@@ -26,3 +26,44 @@ angular.module('starter', ['ionic'])
 */
 
 angular.module('demo', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider
+		.state('signin', {
+			url: '/sign-in',
+			templateUrl: 'templates/sign-in.html',
+			controller: 'SignInCtrl'
+		})
+		.state('tabs', {
+			url: '/tab', 
+			abstract: true,
+			templateUrl: 'templates/tabs.html'
+		})
+		.state('tabs.home', {
+			url: '/home',
+			views: {
+				'home-tab': {
+					templateUrl: 'templates/home.html',
+					controller: 'HomeTabCtrl'
+				}
+			}
+		})
+		.state('tabs.about', {
+			url: '/about',
+			views: {
+				'about-tab': {
+					templateUrl: 'templates/about.html',
+				}
+			}
+		});
+
+	$urlRouterProvider.otherwise('/sign-in');
+})
+.controller('SignInCtrl', function($scope, $state){
+	$scope.signIn = function(user){
+		console.log(user);
+		$state.go('tabs.home');
+	};
+})
+.controller('HomeTabCtrl', function($scope){
+});
